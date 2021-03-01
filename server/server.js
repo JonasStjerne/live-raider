a64bb1c9dbdf5b2fff8014635fb543a510974fa9
Jonas Stjerne <j.stjerne@live.dk>
2021-03-01 11:52:32 +0100

    Handling of pagination/finding streams that meet criteria

--- server/server.js
+++ server/server.js
@@ -1,3 +1,4 @@
+//---------------------Initial Server Setup-------------------------//
 var express = require("express");
 var request = require('request');
 var bodyParser = require('body-parser');
@@ -5,7 +6,11 @@ const port = 8000;
 
 var app = express();
 app.use(bodyParser.json())
-//---------------------Initial Server Setup-------------------------//
+
+// Variables
+const maxViewers = 50; //Max viewers on stream for stream to be added to streamersContainer
+const maxStreamers = 200; //Max streamers to be added to streamersContainer, if not 
+var streamersContainer = []; //Container of streamers
 
 
 //Defining api validation information 
@@ -21,6 +26,7 @@ app.get('/', (req, res) => {
 
 app.listen(port); //Start listening
 
+//Functions to send post and get requests to twitch api, responds with a promise
 function sendPostRequest(req) {
     return new Promise((resolve, reject) => {
         request.post(req, (err, res) => {
@@ -47,31 +53,66 @@ function sendGetRequest(req) {
     })
 }
 
+//Main function, runs async to enable await for a promise from api-call functions
 async function main() {
+    
+    //Get accesstoken
     var twitchTokenRequest = `https://id.twitch.tv/oauth2/token?client_id=${twitch_client_id}&client_secret=${twitch_secret}&grant_type=client_credentials`;
-    let response = await sendPostRequest(twitchTokenRequest);
-    twitchToken = response.access_token;
+    var response = await sendPostRequest(twitchTokenRequest);
+    var twitchToken = response.access_token;
 
+    //Set parameters for get request to get streams
     getStreams = {
         url: `https://api.twitch.tv/helix/streams`,
-        method: "GET",
         headers: {
             "Authorization": "Bearer " + twitchToken,
             "Client-Id": twitch_client_id
         }
     }
 
-    var streams = await sendGetRequest(getStreams);
-    console.log(streams);
+    //While viewercount in first stream in data-response is higher than maxViewers go to next page
+    do {
+        streams = await sendGetRequest(getStreams);
+        getStreams.url = `https://api.twitch.tv/helix/streams?first=100&after=` + streams.pagination.cursor;
+    } while (streams.data[0].viewer_counts >= maxViewers)
 
-        // for (i=0; 3 > i; i++) {
-        //     sendGetRequest(getGames, answer => {
-        //         console.log(answer.data[0]);
-        //         console.log(answer.pagination.cursor)
-        //         afterValue = answer.pagination.cursor;
-        //     })
-        //     await sleep(1000)
-        // }
+    //When viewer_count is => maxViewers go to the page before
+    getStreams.url = `https://api.twitch.tv/helix/streams?first=100&before=` + streams.pagination.cursor;
+    streams = await sendGetRequest(getStreams);
+
+    //Iterate through streams in data-reponse, and add the one with views >= maxViewers to streamersContainer
+    for (let i = streams.data.length - 1; i > 0; i--) {
+        if (streams.data[i].viewer_counts <= maxViewers && streamersContainer.length < maxStreamser) {
+            streamersContainer.push(streams.data[i]);
+            continue;
+        } else if (streams.data[i].viewer_counts > maxViewers) {
+            break;
+        }
+    }
+
+    //Add streams to streamsContainer until maxStreamers is reached
+    while (streamersContainer.length < maxStreamers) {
+        if ((maxStreamers  - streamersContainer.length) < 100) {
+            let streamersContainerSpace = maxStreamers  - streamersContainer.length;
+            getStreams.url = `https://api.twitch.tv/helix/streams?first=${streamersContainerSpace}&after=` + streams.pagination.cursor;
+        } else {
+            getStreams.url = `https://api.twitch.tv/helix/streams?first=100&after=` + streams.pagination.cursor;
+        }
+        var streams = await sendGetRequest(getStreams);
+        streamersContainer.push(streams.data);
+    }
+
+    console.log(streamersContainer);
+    
+
+
+    // for (i=0; 50 > i; i++) {
+    //     var streams = await sendGetRequest(getStreams);
+    //     getStreams.url = `https://api.twitch.tv/helix/streams?first=100&after=` + streams.pagination.cursor;
+    //     console.log(streams.data[0]);
+    // }
+
+
 }
 
 main();
